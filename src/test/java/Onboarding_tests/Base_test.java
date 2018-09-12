268c9af724956a3c6ef6ba17076ee03ffa852163
Bakare <bbakare@deloitte.co.uk>
2018-09-12 12:11:23 +0100

    Added Page Object classes for depositors details, transaction details/summary page with pass through methods for each

--- src/test/java/Onboarding_tests/Base_test.java
+++ src/test/java/Onboarding_tests/Base_test.java
@@ -34,7 +34,7 @@ public class Base_test {
         capabilities.setCapability("platformVersion", "11.2");
         capabilities.setCapability("deviceName", "iPhone 8");
         capabilities.setCapability("app",
-                "/Users/awhitten/Library/Developer/Xcode/DerivedData/Iceberg-dmdimcbnvyefcdajhivuraigymun/Build" +
+                "/Users/bbakare/Library/Developer/Xcode/DerivedData/Iceberg-adtdbektmsxudqdkhkquwuwoeobx/Build" +
                         "/Products/Debug-iphonesimulator/Iceberg.app");
 
         driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
