80cd3aac717383ae2c4f4f13316f1603b15ae13f
Somayeh Alimohammadi <>
2018-09-17 15:41:09 +0100

    Updating CSM package

--- src/main/java/pageObjects/CSM/SupportScreen.java
+++ src/main/java/pageObjects/CSM/SupportScreen.java
@@ -3,20 +3,26 @@ package pageObjects;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSFindBy;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import testData.RandomDataGenerator;
 import utils.GuiCommands;
+
 
 public class SupportScreen extends GuiCommands {
 
     private SetUp setup = new SetUp(driver);
 
-    public Support (IOSDriver driver) {
+    public SupportScreen (IOSDriver driver) {
         super(driver);
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
-    @FindBy (xpath = "(//XCUIElementTypeButton[@name=\"customer_support.start_chat\"])[3]")
+    @iOSFindBy(xpath = "(//XCUIElementTypeButton[@name=\"customer_support.start_chat\"])[2]")  //FIXME get unique accessibility id
     private MobileElement startSupportChatButton;
-    
+
+    @iOSFindBy(xpath = "(//XCUIElementTypeButton[@name=\"Support\"])[2")  //FIXME get unique accessibility id
+    private MobileElement supportButton;
+
 }
